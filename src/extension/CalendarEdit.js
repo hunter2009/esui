df7ed61f711e12ffba33984e478249e18fc0210d
weifeng <weifeng@baidu.com>
2016-07-25 16:56:00 +0800

    modfied some file

--- src/extension/CalendarEdit.js
+++ src/extension/CalendarEdit.js
@@ -17,16 +17,75 @@ define(
         var lib = require('../lib');
         var moment = require('moment');
 
+        var copyStatus = ['rawValue', 'disabled', 'readOnly', 'hidden'];
+
+        var Rule = require('../validator/Rule');
+        var ValidityState = require('../validator/ValidityState');
+
+        /**
+         * 最小值验证规则
+         *
+         * @extends validator.Rule
+         * @class validator.DateRangeRule
+         * @constructor
+         */
+        var DateRangeRule = eoo.create(
+            Rule,
+            {
+                /**
+                 * 规则类型，始终为`"dateRange"`
+                 *
+                 * @type {string}
+                 * @override
+                 */
+                type: 'dateRange',
+
+                /**
+                 * 验证控件的验证状态
+                 *
+                 * @param {string} value 校验值
+                 * @param {Control} control 待校验控件
+                 * @return {validator.ValidityState}
+                 * @override
+                 */
+                check: function (value, control) {
+                    var validate = true;
+                    var errorMessage = '';
+                    var range = control.range;
+                    if (!moment(value, control.paramFormat).isValid()) {
+                         errorMessage = '输入格式错误';
+                        validate = false;
+                    }
+                    else {
+                        if (range) {
+                            var rangeBegin = moment(range.begin);
+                            var rangeEnd = moment(range.end);
+                            var currentDate = moment(value);
+                            if (!(currentDate.diff(rangeBegin) >= 0)
+                                || !(currentDate.diff(rangeEnd) <= 0)) {
+                                validate = false;
+                                errorMessage = '请输入有效范围内的时间';
+                            }
+                        }
+                    }
+                    return new ValidityState(
+                        validate,
+                        errorMessage
+                    );
+                }
+            }
+        );
+
+        esui.registerRule(DateRangeRule, 300);
+
         /**
          * 日历编辑扩展
          *
          * 当日历添加此拓展以后，可以通过输入框来输入具体的日期
          *
-         * TODO: 
          *  1. 复写掉initStructure，来插入的输入框
-         *  2. 添加时间格式验证，失去焦点(不报错，但是不起错用，抛出错误？)或者提交的时候进行格式验证getValidationResult
-         *  3. 对range的范围校验
-         *  4. 禁用和只读的情况
+         *  2. 复写getValidationResult进行格式验证以及range验证
+         *  3. rawValue，以及禁用，只读等状态的切换更改
          * @class extension.CalendarEdit
          * @extends Extension
          * @constructor
@@ -54,9 +113,12 @@ define(
                         return;
                     }
 
-                    // 复写initStructure
+                    // 对一批方法的复写
                     target.initStructure = u.bind(initStructure, target);
                     target.initEvents = u.bind(initEvents, target);
+
+                    // 添加一个校验
+                    target.dateRange = true;
 
                     this.$super(arguments);
                 },
@@ -72,8 +134,12 @@ define(
                         return;
                     }
 
-                    target.initStructure = u.unbind(initStructure, target);
-                    target.initEvents = u.unbind(initEvents, target);
+                    u.unbind(initStructure, target);
+                    u.unbind(initEvents, target);
+                    target.dateRange = false;
+
+                    this.helper.removePartClasses('input');
+                    this.helper.disposeChildren();
 
                     this.$super(arguments);
                 }
@@ -94,24 +160,27 @@ define(
                 controlHelper.replaceMain();
             }
 
-            this.layer.autoCloseExcludeElements = [this.main];
             var template = [
                 '<div class="${classes}">',
-                    '<div data-ui="type:TextBox;childName:input;"></div>',
-                '</div>',
-                '<div class="${arrow}"><span class="${iconCalendar}"></span></div>'
+                    '<div data-ui="type:TextBox;childName:calendarInput;width:130;"></div>',
+                '</div>'
             ];
 
+            this.layer.autoCloseExcludeElements = [this.main];
             this.main.innerHTML = lib.format(
                 template.join(''),
                 {
-                    classes: controlHelper.getPartClassName('text'),
-                    arrow: controlHelper.getPartClassName('arrow'),
-                    iconCalendar: controlHelper.getIconClass()
+                    classes: controlHelper.getPartClassName('textbox'),
+                    arrow: controlHelper.getPartClassName('arrow')
                 }
             );
 
+            controlHelper.addPartClasses('input');
+
             controlHelper.initChildren();
+
+            // 第一次的rawValue赋值监听不到，需要手动触发一次
+            initExtendsionOptions.call(this);
         }
 
         /**
@@ -121,32 +190,15 @@ define(
          * @override
          */
         function initEvents() {
-            this.helper.addDOMEvent(this.main, 'click', u.bind(this.layer.toggle, this.layer));
+            // 输入框的时候，变为hover聚焦更好一点
+            var calendarInput = this.getChild('calendarInput');
+            calendarInput.on('focus', u.bind(this.layer.show, this.layer));
 
-            this.on('change', u.bind(syncMonthView, this));
-            this.getChild('input').on('input', u.bind(syncTextBox, this));
-        }
+            this.on('change', u.bind(syncTextBox, this));
+            calendarInput.on('input', u.bind(syncMonthView, this));
 
-        /**
-         * 更新显示
-         *
-         * @event
-         * @fires Calendar#change
-         * @param {Object} e 事件对象
-         * @param {MonthView} e.target 当前改变值的MonthView控件
-         * @ignore
-         */
-        function syncMonthView(e) {
-            var calendar = this;
-            var calendarTextBox = calendar.getChild('input');
-            var currentElement = document.activeElement;
-            if (currentElement && currentElement !== calendarTextBox.getFocusTarget()) {
-                calendarTextBox.setProperties({
-                    rawValue: u.escape(
-                        moment(calendar.getRawValue()).format(calendar.displayFormat)
-                    )
-                });
-            }
+            // 需要监听property
+            this.on('propertyset', u.bind(syncControlRepainter, this));
         }
 
         /**
@@ -159,32 +211,80 @@ define(
          * @ignore
          */
         function syncTextBox(e) {
+            var calendar = this;
+            setTextBoxProperties.call(this, {
+                rawValue: calendar.getRawValue()
+            });
+        }
+
+        /**
+         * 更新日历的值
+         *
+         * @event
+         * @fires Calendar#change
+         * @param {Object} e 事件对象
+         * @param {MonthView} e.target 当前改变值的MonthView控件
+         * @ignore
+         */
+        function syncMonthView(e) {
             var currTextBox = e.target;
             var date = currTextBox.getRawValue();
             if (!date) {
                 return;
             }
 
-            var rangeBegin = moment(this.range.begin, this.paramFormat);
-            var rangeEnd = moment(this.range.end, this.paramFormat);
-            var currentDate = moment(date, this.paramFormat);
-            // ?bug
-            if (currentDate.isValid()
-                && currentDate.diff(rangeBegin) >= 0
-                && currentDate.diff(rangeEnd) >= 0
-            ) {
-            // if (/^(\d{1,4})(-|.|\/)(\d{1,2})\2(\d{1,2})$/.test(date)) {
-                if (moment(date, this.paramFormat).isValid()) {
-                    this.setProperties({
-                        rawValue: moment(date).toDate() || ''
-                    });
-                }
-            // }
+            if (moment(date, this.paramFormat).isValid()) {
+                this.setProperties({
+                    rawValue: moment(date).toDate()
+                });
             }
         }
 
-        function validateDate(date) {
+        /**
+         * 需要去监听properties changes来实现拓展状态属性的切换
+         * @param {Object} e 事件对象
+         * @ignore
+         */
+        function syncControlRepainter(e) {
+            var changes = e.changes;
+            if (changes.length) {
+                var properties = {};
+                u.each(changes, function(change) {
+                    if (u.contains(copyStatus, change.name)) {
+                        properties[change.name] = change.newValue;
+                    }
+                });
+                setTextBoxProperties.call(this, properties);
+            }
+        }
 
+        /**
+         * 初始化输入框的的value以及各种状态值
+         * 因为initOptions生命周期在initExtensions之前，所以需要手动触发一次
+         * @param {Object} e 事件对象
+         * @ignore
+         */
+        function initExtendsionOptions() {
+            setTextBoxProperties.call(this, u.pick(this, copyStatus));
+        }
+
+        /**
+         * 设置输入框的属性
+         * 如果当前输入框获取焦点则认为该行为由输入框触发，不进行任何处理
+         * @param {Object} properties 待设置的属性
+         * @ignore
+         */
+        function setTextBoxProperties(properties) {
+            var calendarInput = this.getChild('calendarInput');
+            var value = properties.rawValue;
+            if (value) {
+                properties.rawValue = moment(value).format(this.displayFormat);
+            }
+            // 当前行为
+            var currentElement = document.activeElement;
+            if (currentElement && currentElement !== calendarInput.getFocusTarget()) {
+                calendarInput.setProperties(properties);
+            }
         }
 
         esui.registerExtension(CalendarEdit);
